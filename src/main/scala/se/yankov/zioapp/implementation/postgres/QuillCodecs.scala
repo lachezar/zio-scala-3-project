f7f5a6aaa66fd151a0b48a55e6e4e045cffaee1f
lachezar <lucho870601@gmail.com>
2023-12-15 14:40:22 +0100

    Replaced the Quill codecs with generic MappedEncoding piggy backing on the Opq typeclass

--- src/main/scala/se/yankov/zioapp/implementation/postgres/QuillCodecs.scala
+++ src/main/scala/se/yankov/zioapp/implementation/postgres/QuillCodecs.scala
@@ -3,22 +3,14 @@ package implementation
 package postgres
 
 import java.sql.Types
-import java.util.UUID
 
 import DbContext.*
-import io.getquill.JsonbValue
+import io.getquill.{ JsonbValue, MappedEncoding }
 
 object QuillCodecs:
 
-  given uuidDecoder[Out](using opq: Opq[UUID, Out]): Decoder[Out] =
-    decoder(row => index => opq.pack(UUID.fromString(row.getObject(index).toString)))
-  given uuidEncoder[Out](using opq: Opq[UUID, Out]): Encoder[Out] =
-    encoder(Types.OTHER, (index, value, row) => row.setObject(index, opq.unpack(value), Types.OTHER))
-
-  given bigDecimalDecoder[Out](using opq: Opq[BigDecimal, Out]): Decoder[Out] =
-    decoder(row => index => opq.pack(row.getBigDecimal(index)))
-  given bigDecimalEncoder[Out](using opq: Opq[BigDecimal, Out]): Encoder[Out] =
-    encoder(Types.NUMERIC, (index, value, row) => row.setBigDecimal(index, opq.unpack(value).bigDecimal))
+  given [In, Out](using opq: Opq[In, Out]): MappedEncoding[Out, In] = MappedEncoding[Out, In](_.unpack)
+  given [In, Out](using opq: Opq[In, Out]): MappedEncoding[In, Out] = MappedEncoding[In, Out](opq.pack(_))
 
   // Fix for jsonb columns that can be null https://github.com/zio/zio-protoquill/issues/283#issuecomment-1627799047
   given [T]: Encoder[Option[JsonbValue[T]]] = JdbcEncoder[Option[JsonbValue[T]]](
