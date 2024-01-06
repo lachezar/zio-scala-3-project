b79c9c0e35b71d7afd0dab87caf69b1cf52b2b78
lachezar <lucho870601@gmail.com>
2024-01-06 15:21:43 +0100

    deps update

--- src/main/scala/se/yankov/zioapp/AppConfig.scala
+++ src/main/scala/se/yankov/zioapp/AppConfig.scala
@@ -1,15 +1,12 @@
 package se.yankov.zioapp
 
 import zio.*
-import zio.config.magnolia.descriptor
-import zio.config.syntax.*
-import zio.config.toKebabCase
-import zio.config.typesafe.TypesafeConfig
+import zio.config.ConfigErrorOps
+import zio.config.magnolia.deriveConfig
+import zio.config.typesafe.TypesafeConfigProvider
 
 import implementation.kafka.KafkaConfig
 import implementation.postgres.DbConfig
-
-import scala.io.Source
 
 type ConfigEnv = DbConfig & KafkaConfig
 
@@ -17,10 +14,6 @@ final case class AppConfig(db: DbConfig, kafka: KafkaConfig)
 
 object AppConfig:
   val layer: TaskLayer[ConfigEnv] =
-    ZLayer(ZIO.attempt(Source.fromResource("application.conf").mkString))
-      .flatMap(content =>
-        val configLayer = TypesafeConfig
-          .fromHoconString(content.get, descriptor[AppConfig].mapKey(toKebabCase))
-          .mapError(e => new RuntimeException(e.prettyPrint()))
-        configLayer.narrow(_.db) ++ configLayer.narrow(_.kafka)
-      )
+    val configLayer = ZLayer(TypesafeConfigProvider.fromResourcePath().kebabCase.load(deriveConfig[AppConfig]))
+      .mapError(e => new RuntimeException(e.prettyPrint()))
+    configLayer.project(_.kafka) ++ configLayer.project(_.db)
